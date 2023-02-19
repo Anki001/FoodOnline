e6ee7ea46a7998d0983c486537ecc697841706bc
Ankush Ashok <ankush_gawande@jabil.com>
2023-02-19 22:45:03 +0800

    Ankush - Configure API scope resource and clients for identity server

---
+++ Services/FoodOnline.Services.Identity/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FoodOnline.Services.Identity.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+    }
+}
---
+++ Services/FoodOnline.Services.Identity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoodOnline.Services.Identity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
