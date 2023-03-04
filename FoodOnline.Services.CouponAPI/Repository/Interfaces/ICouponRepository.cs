3b5d95d93b8d0fdbfe5a220af155b123aa324ecf
Ankush Ashok <ankush_gawande@jabil.com>
2023-03-04 15:57:06 +0800

    Ankush - Coupon Service

---
+++ FoodOnline.Services.CouponAPI/Repository/Interfaces/ICouponRepository.cs
@@ -0,0 +1,9 @@
+﻿using FoodOnline.Services.CouponAPI.Models.Dtos;
+
+namespace FoodOnline.Services.CouponAPI.Repository.Interfaces
+{
+    public interface ICouponRepository
+    {
+        Task<CouponDto> GetCouponByCode(string couponCode);
+    }
+}
